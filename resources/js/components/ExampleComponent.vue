e7598c12cee6a7907de3e1f2ef69c234699f7fd8
root <root@194-67-78-176.cloudvps.regruhosting.ru>
2020-06-21 18:14:43 +0300

    del hello world

--- resources/js/components/ExampleComponent.vue
+++ resources/js/components/ExampleComponent.vue
@@ -15,7 +15,7 @@
 
 <script>
     export default {
-        data() { 
+        data() {
             return {
                 count: 0
             }
--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    event(new \App\Events\UserEvent('hello world'));
     return view('welcome');
 });
 
